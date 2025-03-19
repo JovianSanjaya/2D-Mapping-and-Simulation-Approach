9602a754f92220174f30dcd88315598fc99051ae
jovian <jovian14.22@ichat.sp.edu.sg>
2025-03-19 13:19:04 +0800

    web

--- Hack/app.py
+++ Hack/app.py
@@ -1,20 +1,12 @@
 from flask import Flask, render_template, redirect, url_for
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt  
 
 app = Flask(__name__)
 
-@app.route("/data_analysis")
-def data_analysis():
-    return render_template("data_analysis.html")
-
-@app.route("/ai_prediction")
-def ai_prediction():
-    return render_template("ai_prediction.html")
-
 @app.route("/")
 def home():
-    return redirect(url_for('data_analysis'))
+    return render_template("index.html")  
 
 if __name__ == "__main__":
     app.run(debug=True)
